426604e42c802b445b8279c50cf1992024da8cd0
Abha Jha <88975365+abha06@users.noreply.github.com>
2023-01-12 16:16:11 +0530

    Update Modal.js
    added modal to this

--- src/component/modal/Modal.js
+++ src/component/modal/Modal.js
@@ -1,47 +1,85 @@
-import React from "react";
-import "./modal.css";
-import styled from "styled-components";
+import React, { useEffect, useState} from 'react'
+import styled ,{keyframes} from "styled-components";
+//import { FaArrowAltCircleUp } from "react-icons/fa";
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
-const Modal = ({ setOpenModal }) => {
-    return (
-        <Wraper1>
-            <>
-        <div className='container1 '>
-        <button type="button" class="btn btn-primary ms-3 mb-4" data-dismiss="modal" id="cancelBtn" onClick={() => { setOpenModal(false) }}>X</button>
-        <h3 className=' text-center mb-0'>Get Free Live Demo</h3>
-        <div className='row'>
+const Modal = () => {
+  const [isVisible ,setIsVisible] = useState(false);
+  
+  
+ const setOpenModal = () =>{
+  setIsVisible(false);
+  };
+ 
+  const ListenToScroll = () =>{
+   
+    let heightToHidden = 700;
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    if(winScroll > heightToHidden ){
+      setIsVisible(true);
+    }else{
+      setIsVisible(false);
+    }
     
-            <div className='col-md-6'>
-                <img src="assets/img/modal-img.webp" height="300px" alt="/" />
-            </div>
-            <div className='col-md-6'>
-                <form className='p-2'>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name here" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Phone</label>
-                        <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Your Phone Number" />
-                    </div>
-                    <button type="button" class="btn btn-warning mb-4">Contact</button>
-                    <button type="button" class="btn btn-primary ms-3 mb-4" data-dismiss="modal" id="cancelBtn" onClick={() => {
-                        setOpenModal(false)
-                    }}>Close</button>
-                </form>
-            </div>
+  
+  };
+  
+
+  useEffect(() => {
+    window.addEventListener('scroll' , ListenToScroll);
+    return () => window.removeEventListener("scroll" ,ListenToScroll);
+  }, [])
+  
+  return (
+    <Wraper1>
+      {isVisible && (
+        <>
+         
+      <div className='container1 '>
+      <h3 className=' text-center mb-0 mt-2'>Get Free Live Demo</h3>
+       <div className='row'>
+        
+        <div className='col-md-6'>
+          <img src="assets/img/modal-img.webp" height="300px" alt="/"/>
         </div>
-    
-    </div>
-    </>
-    </Wraper1>
-    );
-};
+        <div className='col-md-6'>
+          <form className='p-2'>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Name</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name here"/>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Phone</label>
+            <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Your Phone Number"/>
+          </div>
+          
+                    <button type="button" className='btn btn-secondary rounded ms-3 mb-4 ms-md-1 p-md-2 ps-md-3 pe-md-3'>Send Message</button>
+                    <button type="button" class="btn btn-primary ms-3 mb-4" data-dismiss="modal" id="cancelBtn" onClick={() => {
+                        setOpenModal()
+                    }}>Close</button>
+          </form>
+        </div>
+       </div>
+        
+      </div>
+      </>
+      )}
 
+    </Wraper1>
+    
+  )};
+
+  const slideAnim = (top, left) => keyframes`
+  100% {
+    top: ${ top };
+    left: ${ left };
+  }  
+`;
 const Wraper1 = styled.section`
 
   .container1 {
@@ -59,23 +97,23 @@ const Wraper1 = styled.section`
     align-items: center;
     cursor: pointer;
     background: linear-gradient(to bottom, #ace8fc 0%, #f3aed0 100%);
-    
-    }
-    
+    &.slide {
+
+      animation: ${slideAnim('20px', '30px')} 0.4s ease-in-out forwards;
+  
+      @media (max-width: ${600}px) {
+        .container1{
+          height:200px;
+          width:300px;
+        }
+      }  
+   }
+  
   }
   
     }
   }
-  @media only screen and (max-width: 500px) {
-    .container1{
-      font-size: 1.2rem;
-      height:400px;
-      width:500px;
-      bottom:50px;
-  }
+  
 `;
 
-
-export default Modal;
-
-
+export default Modal
